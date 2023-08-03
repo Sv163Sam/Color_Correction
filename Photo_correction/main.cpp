3beaf5c028160b1740ae3e0f28d7dfa65838f389
Sv163Sam <Tankruuus@gmail.com>
2023-08-03 15:59:55 +0400

    Change logic, Add comments, exam-images

--- Photo_correction/main.cpp
+++ Photo_correction/main.cpp
@@ -1,65 +1,42 @@
 #include "main.h"
 
-using namespace cv;
-using namespace std;
-
-Mat color_correction(Mat& img, cv::Scalar &balance)
+void color_alignment(cv::Mat& img1, cv::Mat& img2)
 {
-    // Split image into R, G, B channels
-    Mat channels[3];
-    split(img, channels);
+    cv::Scalar mean1, mean2, stddev1, stddev2;
 
-    // Calculate mean brightness for each channel
-    double mean_R = mean(channels[0])[0];
-    double mean_G = mean(channels[1])[0];
-    double mean_B = mean(channels[2])[0];
+    // Calculate channels avg value
+    meanStdDev(img1, mean1, stddev1);
+    meanStdDev(img2, mean2, stddev2);
 
-    // Calculate correction coefficients for each channel
-    double coeff_R = (mean_R + mean_G + mean_B) / (1.5 * mean_R);
-    double coeff_G = (mean_R + mean_G + mean_B) / (1.5 * mean_G);
-    double coeff_B = (mean_R + mean_G + mean_B) / (1.5 * mean_B);
-
-    // Apply correction to each pixel in each channel
-    channels[0] = (channels[0] * coeff_R + balance[0])/2 ;
-    channels[1] = (channels[1] * coeff_G + balance[1])/2 ;
-    channels[2] = (channels[2] * coeff_B + balance[2])/2 ;
-
-    // Merge corrected channels back into image
-    Mat corrected_img;
-    merge(channels, 3, corrected_img);
-
-    // Display original and corrected images
-    return corrected_img;
+    // Color normalization
+    for (int i = 0; i < img1.rows; i++) {
+        for (int j = 0; j < img1.cols; j++) {
+            auto& pixel1 = img1.at<cv::Vec3b>(i, j);
+            pixel1[0] = cv::saturate_cast<uchar>((pixel1[0] - mean1[0]) * (stddev2[0] / stddev1[0]) + mean2[0]);
+            pixel1[1] = cv::saturate_cast<uchar>((pixel1[1] - mean1[1]) * (stddev2[1] / stddev1[1]) + mean2[1]);
+            pixel1[2] = cv::saturate_cast<uchar>((pixel1[2] - mean1[2]) * (stddev2[2] / stddev1[2]) + mean2[2]);
+        }
+    }
 }
 
 int main()
 {
-
-    //cv::Mat image1 = cv::imread("img/Ex1.png");
-    //cv::Mat image2 = cv::imread("img/Ex2.png");
-
-    //cv::Mat image1 = cv::imread("img/Green1.png");
-    //cv::Mat image2 = cv::imread("img/Green2.png");
-
-    cv::Mat image1 = cv::imread("img/img_1.png");
-    cv::Mat image2 = cv::imread("img/img_2.png");
-
-    //cv::Mat image1 = cv::imread("img/Street1.png");
-    //cv::Mat image2 = cv::imread("img/Street2.png");
+    cv::Mat img1 = cv::imread("img/Street1.png");
+    cv::Mat img2 = cv::imread("img/Street2.png");
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    cv::Scalar desiredBalance = (mean(image1) + mean(image2)) / 2;
-
-    image1 = color_correction(image1, desiredBalance);
-    image2 = color_correction(image2, desiredBalance);
+    //
+    // Change img1, img2 position to change color addiction
+    //
+    color_alignment(img2, img1);
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
     std::cout << "Execution time: " << duration_ms << " ms" << std::endl;
 
-    imwrite("corrected_img/cor_img1.png", image1);
-    imwrite("corrected_img/cor_img2.png", image2);
+    imwrite("corrected_img/Cor_random1.png", img1);
+    imwrite("corrected_img/Cor_random2.png", img2);
 
     return 0;
 }
--- Photo_correction/main.h
+++ Photo_correction/main.h
@@ -2,14 +2,6 @@
 #define MAIN_H
 
 #include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
 #include <opencv2/opencv.hpp>
-#include <ctime>
-#include <pthread.h>
-#include <cmath>
-#include <opencv2/xphoto.hpp>
-#include <opencv2/imgproc.hpp>
 
 #endif //MAIN_H
